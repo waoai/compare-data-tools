9d776e0ec04f7f63a388d0496eee2f405d6239f0
seveibar <seveibar@gmail.com>
2020-02-25 03:26:06 -0500

    Updates

---
+++ precache-manifest.4c31816b22b7670677b71b4334362986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16ad189fbc37f063f977",
+    "url": "/compare-data-tools/static/css/main.b3332d72.chunk.css"
+  },
+  {
+    "revision": "16ad189fbc37f063f977",
+    "url": "/compare-data-tools/static/js/main.16ad189f.chunk.js"
+  },
+  {
+    "revision": "5633722fa7ea1564a167",
+    "url": "/compare-data-tools/static/css/1.2ed9f3b5.chunk.css"
+  },
+  {
+    "revision": "5633722fa7ea1564a167",
+    "url": "/compare-data-tools/static/js/1.5633722f.chunk.js"
+  },
+  {
+    "revision": "1144cb6e2177ae8b9846",
+    "url": "/compare-data-tools/static/js/runtime~main.1144cb6e.js"
+  },
+  {
+    "revision": "bc9cf063f37813f606528434fe306196",
+    "url": "/compare-data-tools/index.html"
+  }
+];
